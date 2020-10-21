8a9299bbd3d63b454186b23d7e96242a73ec996f
Matthew Machurek <mrjm2005@yahoo.com>
2020-10-21 15:41:49 -0500

    Got the eventHub working finally

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,6 +1,8 @@
 import { getCriminals, useCriminals } from './CriminalProvider.js'
 import { Criminal } from './Criminal.js'
+import { useConvictions } from '../convictions/ConvictionProvider.js'
 
+const eventHub = document.querySelector(".container")
 const criminalsContainer = document.querySelector(".criminalsContainer")
 
 export const CriminalList = () => {
@@ -10,8 +12,10 @@ export const CriminalList = () => {
             component should be rendered?
         */
        const criminalArray = useCriminals()
-       let criminalHTMLRepresentation = ""
-        for (const criminal of criminalArray) {
+
+    let criminalHTMLRepresentation = ""
+       
+       for (const criminal of criminalArray) {
 
         criminalHTMLRepresentation += Criminal(criminal)
 
@@ -23,3 +27,36 @@ export const CriminalList = () => {
         }
     })
 }
+
+
+eventHub.addEventListener("crimeChosen", event => {
+   console.log(event)
+        if (event.detail.crimeThatWasChosen !== "0") {
+                //debugger 
+            const criminalArray = useCriminals()
+
+            const convictionsArray = useConvictions()
+
+            const findConviction = convictionsArray.find(convictionObj => {
+                return convictionObj.id === event.detail.crimeThatWasChosen
+                
+            })
+            //console.log(findConviction)
+            const filterCriminal = criminalArray.filter(criminalObj => {
+                return criminalObj.conviction === findConviction.name
+            })
+
+                let criminalHTMLRepresentation = ""
+       
+                for (const criminal of filterCriminal) {
+         
+                 criminalHTMLRepresentation += Criminal(criminal)
+            }
+                 criminalsContainer.innerHTML = `
+                 <section class="criminalList">
+                     ${criminalHTMLRepresentation}
+                 </section>
+                 `
+                 
+    }
+})
